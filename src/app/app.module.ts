ed5311aebe1ff9e0b3830b8b6908041921509161
Takashi Yamashina <takashi.yamashina@gmail.com>
2023-08-07 21:58:58 +0900

    create SharedModule and UserModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TitleComponent } from './shared/components/title/title.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, TitleComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
